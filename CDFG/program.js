a661fba8603794074309ccd77491d2d012da5041
Project_HSI <57279339+ProjectHSI@users.noreply.github.com>
2021-08-09 19:51:55 +1000

    Last first day commit - 9/08/2021 > 10/08/2021

--- CDFG/program.js
+++ CDFG/program.js
@@ -23,7 +23,10 @@ canvas.height = CANVAS_HEIGHT;
 document.body.appendChild(canvas);
 
 var nanonautImage = new Image();
-nanonautImage.src = "/Images/nanonaut.png";
+nanonautImage.src = "Images/nanonaut.png";
+
+var backgroundImage = new Image();
+backgroundImage.src = "Images/background.png";
 
 var nanonautX = 50;
 var nanonautY = 40;
@@ -57,8 +60,20 @@ function update() {
 -----------
 */
 function draw() {
-    c2d.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT)
+    
+    // Draw the sky. - Book
+    c2d.fillStyle = "LightSkyBlue"
+    c2d.fillRect(0, 0, CANVAS_WIDTH, GROUND_Y - 40)
+
+    // Draw the background. - Book
+    c2d.drawImage(backgroundImage, 0, -210)
+
+    // Draw the ground. - Book
+    c2d.fillStyle = "ForestGreen"
+    c2d.fillRect(0, GROUND_Y - 40, CANVAS_WIDTH, CANVAS_HEIGHT - GROUND_Y + 40)
 
     // Draw the Nanonaut. - Book
     c2d.drawImage(nanonautImage, nanonautX, nanonautY)
 }
+
+// Continue on page 39. --- All commits submitted on 10/08/2021 are Second Day Coding. --- Written on the first day of coding.
