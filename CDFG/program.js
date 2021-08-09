d1693880318aab96b46ba0da5c86f8c3dd36933f
Project_HSI <57279339+ProjectHSI@users.noreply.github.com>
2021-08-09 18:41:07 +1000

    Changes to main program.js [CDFG] to progress with the book.

--- CDFG/program.js
+++ CDFG/program.js
@@ -1,19 +1,53 @@
-var canvas = document.createElement('canvas')
-canvas.width = 1920
-canvas.height = 1080
+/*
+-------------
+- CONSTANTS -
+-------------
+*/
+const CANVAS_WIDTH = 800;
+const CANVAS_HEIGHT = 600;
+const NANNAUT_WIDTH = 181;
+const NANONAUT_HEIGHT = 229;
+const GROUND_Y = 540;
+/*
+---------
+- SETUP -
+---------
+*/
+var canvas = document.createElement('canvas');
+var c2d = canvas.getContext("2d");
+var c3d = canvas.getContext("webgl2");
 
-var c2d = canvas.getContext("2d")
-var c3d = canvas.getContext("webgl2")
+canvas.width = CANVAS_WIDTH;
+canvas.height = CANVAS_HEIGHT;
 
-var image = new Image()
-image.src = "Images/nanonaut.png"
+document.body.appendChild(canvas);
 
-window.addEventListener('load', start)
+var nanonautImage = new Image();
+nanonautImage.src = "/Images/nanonaut.png";
 
-function start () {
-    c2d.fillStyle = "yellow";
-    c2d.fillRect(10,10,30,30)
-    c2d.drawImage(image, 20, 40)
+var nanonautX = 50;
+var nanonautY = 40;
+
+window.addEventListener('load', start);
+
+function start() {
+    window.requestAnimationFrame(mainLoop);
 }
-
-document.body.appendChild(canvas)
+/*
+-------------
+- MAIN LOOP -
+-------------
+*/
+function mainLoop() {
+    update();
+    draw();
+    window.requestAnimationFrame(mainLoop);
+}
+/*
+---------
+- UPDATING -
+---------
+*/
+function update() {
+    // No update function yet lol
+}
